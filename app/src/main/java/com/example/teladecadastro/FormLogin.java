87b91da0476869e033eab32dec6fe534a213f34d
flaviogustavorj <flaviogustavorj@gmail.com>
2022-07-30 00:17:05 -0300

    Tela de cadastro
    Tela de cadastro implementada

--- app/src/main/java/com/example/teladecadastro/FormLogin.java
+++ app/src/main/java/com/example/teladecadastro/FormLogin.java
@@ -1,9 +1,14 @@
 package com.example.teladecadastro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +16,19 @@ public class FormLogin extends AppCompatActivity {
         setContentView(R.layout.activity_form_login);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
